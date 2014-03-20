8b2e7e3765b7ef3ff575288cd1c67777513fcc82
Chester Kener <Cl3Kener@gmail.com>
2014-03-20 08:29:10 -0400

    cpufreq: PegasusQ
    Signed-off-by: Chester Kener <Cl3Kener@gmail.com>
    
    Conflicts:
    	drivers/cpufreq/Makefile
    	include/linux/cpufreq.h
    
    Conflicts:
    	arch/arm/configs/deathly_d2_defconfig

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -387,7 +387,9 @@ extern struct cpufreq_governor cpufreq_gov_ondemandplus;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIMM)
 extern struct cpufreq_governor cpufreq_gov_intellimm;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_intellimm)
->>>>>>> 4705efa... cpufreq_intellimm: intelliminmax cpu governor initial baseline commit
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_PEGASUSQ)
+extern struct cpufreq_governor cpufreq_gov_pegasusq;
+#define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_pegasusq)
 #endif
 
 
